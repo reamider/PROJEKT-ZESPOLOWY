34dd1b93099f9e66a8aac96d892689fe9e937eee
damian11 <damian.kasztelan1@op.pl>
2016-01-12 02:08:28 +0100

    edycja index.html

--- APP/scripts.js
+++ APP/scripts.js
@@ -2,24 +2,28 @@ $(document).ready(function(){
 
   var body = $("body");
   var div = $("<div>");
+    var div2 =$("div2")
   var img = $("img");
   var center =$("center");
+
 //dodanie elementu div do elementu body
   body.append(div);
+     body.append(div2);
    body.append(img);
    body.append(center);
+   body.append(div2);
+
   //metody jquery
   //dodanie tekstu
-  div.text("SYLWETKA");
+// div.text("Kliknij na wybraną partie mieśni aby wybrać ćwiczenia");
 //  dodanie klasy npis
+div2.animate({"left": "30%"}, "slow");
   div.addClass("napis");
   div.css("display","none");
 // zmiana przezroczystości od 0 do 1
- div.fadeIn(8000);
+ div.fadeIn(2000);
 
 img.css("display", "none");
-img.fadeIn(4999);
+img.fadeIn(1000);
 
-center.css("display", "none");
-center.fadeIn(200);
 });
